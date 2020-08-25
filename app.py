8bed78c03ff6ec91b6cfe4f304f8382d61c7b12f
Tom Draper <tomjdraper1@gmail.com>
2020-08-25 11:18:22 +0100

    Home page

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
     return render_template('home.html')
+
+@app.route("/liverpool")
+def liverpool():
+    return render_template('team.html')
+
+@app.route("/manchester-city")
+def manchesterCity():
+    return render_template('team.html')
+
+@app.route("/manchester-united")
+def manchesterUnited():
+    return render_template('team.html')
+
+@app.route("/chelsea")
+def chelsea():
+    return render_template('team.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
