b5cba7aa4f61fe8f7960228b8cb40ea9d325f495
Tom Draper <tomjdraper1@gmail.com>
2020-08-25 11:58:52 +0100

    Team page

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', team="None")
 
 @app.route("/liverpool")
-def liverpool():
-    return render_template('team.html')
-
 @app.route("/manchester-city")
-def manchesterCity():
-    return render_template('team.html')
-
 @app.route("/manchester-united")
-def manchesterUnited():
-    return render_template('team.html')
-
 @app.route("/chelsea")
-def chelsea():
-    return render_template('team.html')
+@app.route("/leicester")
+@app.route("/spurs")
+@app.route("/wovles")
+@app.route("/arsenal")
+@app.route("/sheffield-united")
+@app.route("/burnley")
+@app.route("/southampton")
+@app.route("/everton")
+@app.route("/newcastle")
+@app.route("/crystal-palace")
+@app.route("/brighton")
+@app.route("/west-ham")
+@app.route("/aston-villa")
+@app.route("/leeds-united")
+@app.route("/west-brom")
+@app.route("/fulham")
+def team():
+    rule = request.url_rule
+    team_name = rule.rule[1:]
+    return render_template('team.html', team=team_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
