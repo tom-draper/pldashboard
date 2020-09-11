73fcf3810c65e0a2738c94e5eed593c128478ada
Tom Draper <tomjdraper1@gmail.com>
2020-09-11 20:30:58 +0100

    Update fixtures graphs

--- data.py
+++ data.py
@@ -14,10 +14,23 @@ class Data:
 
     def __init__(self, current_season):
         self.season = current_season
-        self.last_standings = pd.DataFrame()
-        self.team_ratings = pd.DataFrame()
+        
+        self.standings = pd.DataFrame()
         self.fixtures = pd.DataFrame()
+        self.team_ratings = pd.DataFrame()
+        self.home_advantage = self.updateHomeAdvantage()
     
+    
+    # ---------- Home Advantage Data ------------
+    
+    def updateHomeAdvantage(self):
+        # TODO : Caculate home advantage percentage for each team from last n seasons
+        # https://help.smarkets.com/hc/en-gb/articles/115000647291-Why-you-should-consider-home-advantage-for-football-trading
+        return 0.492 - 0.287  # 0.205
+
+
+    # ---------- Standings Data ------------
+
     def standingsData(self, season, request_new=True):
         if request_new:
             response = requests.get(self.url + 'competitions/PL/standings/?season={}'.format(season), 
@@ -33,7 +46,7 @@ class Data:
             with open(f'data/standings_{season}.json', 'r') as json_file:
                 return json.load(json_file)
 
-    def getLastStandings(self, no_seasons):
+    def getStandings(self, no_seasons):
         """Get the Premier League table standings from the last specified number of 
            seasons. Compile each of these standings into a single dataframe to return.
            Dataframe contains only teams that are members of the current season.
@@ -51,9 +64,7 @@ class Data:
 
         # Loop from current season to the season 2 years ago
         for i in range(no_seasons):
-            
             data = self.standingsData(self.season-i, request_new=False)
-            
             # pprint.pprint(data)
             df = pd.DataFrame(data)
 
@@ -61,10 +72,6 @@ class Data:
             team_names = pd.Series([df['team'][x]['name']
                                     for x in range(len(df))])
             df['team'] = team_names
-
-            # If we're using current season data, save list of team names to object team dataframe
-            # if i == 0:
-            #     self.team_ratings = pd.DataFrame({'Team': team_names})
 
             # Rename columns to standardised names
             df.rename(columns={'position': f'Position_{self.season-i}', 
@@ -94,7 +101,10 @@ class Data:
         standings.reset_index(drop=True, inplace=True)
 
         return standings
-    
+
+
+    # ------------ Fixtures Data -------------
+
     def fixturesData(self, request_new=True):
         if request_new:
             response = requests.get(self.url + 'competitions/PL/matches/?season={}'.format(self.season),
@@ -110,7 +120,6 @@ class Data:
         else:
             with open('data/fixtures.json', 'r') as json_file:
                 return json.load(json_file)
-        
 
     def getFixtures(self):
         data = self.fixturesData(request_new=False)
@@ -141,69 +150,103 @@ class Data:
         
         return fixtures
                    
+
+
+
+
+    # ----------- Team Ratings Data -----------
+    
     def calcRating(self, position, points, gd):
         rating = (20 - position) / 2
         if gd != 0:
             rating *= gd
         if points != 0:
             rating *= points
-
         return rating
-
+    
     def getSeasonWeightings(self, no_seasons, current_weight=0.7):
-        return [current_weight] + [0.25, 0.05]
+        # TODO : generate list of appropriate size
+        return [current_weight, 0.25, 0.05]
+    
+    def getTeamRatings(self, no_seasons, standings):
+        if standings.empty:
+            standings = self.getStandings(no_seasons)
         
-        
-    def updateFixtures(self, no_seasons, team=None):
-        self.last_standings = self.getLastStandings(no_seasons)
-        self.fixtures = self.getFixtures()
-
+        team_ratings = pd.DataFrame()
         # Add current season team names to the object team dataframe
-        self.team_ratings['Team'] = self.last_standings['Team']
+        team_ratings['Team'] = standings['Team']
 
         # Create column for each included season
         for i in range(0, no_seasons):
-            self.team_ratings[f'Rating {i}Y Ago'] = np.nan
+            team_ratings[f'Rating {i}Y Ago'] = np.nan
         
         # Insert rating values for each row
-        for idx, row in self.last_standings.iterrows():
+        for idx, row in standings.iterrows():
             for i in range(no_seasons):
                 if i == 0: # TODO: REMOVE ONCE SEASON STARTED
                     rating = (20 - row[f'Position_{self.season-1}']) / 2
                 else:
                     rating = self.calcRating(row[f'Position_{self.season-i}'], row[f'Points_{self.season-i}'], row[f'GD_{self.season-i}'])
-                self.team_ratings.loc[self.team_ratings.loc[self.team_ratings['Team'] == row['Team']].index, 'Rating {}Y Ago'.format(i)] = rating
+                team_ratings.loc[team_ratings.loc[team_ratings['Team'] == row['Team']].index, 'Rating {}Y Ago'.format(i)] = rating
 
         # Replace any NaN with the lowest rating in the same column
-        for col in self.team_ratings.columns:
-            self.team_ratings[col].replace(np.nan, self.team_ratings[col].min(), inplace=True)
+        for col in team_ratings.columns:
+            team_ratings[col].replace(np.nan, team_ratings[col].min(), inplace=True)
 
         # Create normalised versions of the three ratings columns
         for i in range(0, no_seasons):
-            self.team_ratings[f'Normalised Rating {i}Y Ago'] = (self.team_ratings[f'Rating {i}Y Ago'] - self.team_ratings[f'Rating {i}Y Ago'].min()) / (self.team_ratings[f'Rating {i}Y Ago'].max() - self.team_ratings[f'Rating {i}Y Ago'].min())
+            team_ratings[f'Normalised Rating {i}Y Ago'] = (team_ratings[f'Rating {i}Y Ago'] - team_ratings[f'Rating {i}Y Ago'].min()) / (team_ratings[f'Rating {i}Y Ago'].max() - team_ratings[f'Rating {i}Y Ago'].min())
 
         w = self.getSeasonWeightings(no_seasons) # Column weights
-        self.team_ratings['Total Rating'] = 0
+        team_ratings['Total Rating'] = 0
         for i in range(0, no_seasons):
-            self.team_ratings['Total Rating'] += w[i] * self.team_ratings[f'Normalised Rating {i}Y Ago']
+            team_ratings['Total Rating'] += w[i] * team_ratings[f'Normalised Rating {i}Y Ago']
 
-        self.team_ratings.sort_values(by="Total Rating", ascending=False, inplace=True)
-        self.team_ratings.rename(columns={'Rating 0Y Ago': 'Rating Current', 'Normalised Rating 0Y Ago': 'Normalised Rating Current'})
+        team_ratings.sort_values(by="Total Rating", ascending=False, inplace=True)
+        team_ratings.rename(columns={'Rating 0Y Ago': 'Rating Current', 'Normalised Rating 0Y Ago': 'Normalised Rating Current'})
+        
+        return team_ratings
+        
+    
+    # ----------- Update Plotly Graph HTML Files ------------    
+    
+    def updateFixtures(self, no_seasons, standings, fixtures, team_ratings, team=None):
+        if standings.empty:
+            standings = self.getStandings(no_seasons)
+        if fixtures.empty:
+            fixtures = self.getFixtures()
+        if team_ratings.empty:
+            team_ratings = self.getTeamRatings()
 
         # Input team rating dataframe to grade upcoming fixtures
         gdv = GenDataVis()
         if team == None:
-            for team_name in self.last_standings['Team']:
-                gdv.genFixturesGraph(team_name, self.fixtures, self.team_ratings)
+            for team_name in self.standings['Team']:
+                gdv.genFixturesGraph(team_name, self.fixtures, self.team_ratings, self.home_advantage)
         else:
-            gdv.genFixturesGraph(team, self.fixtures, self.team_ratings)
+            gdv.genFixturesGraph(team, self.fixtures, self.team_ratings, self.home_advantage)
     
-    def updateAll(self, fixtures_no_seasons):
-        data.updateFixtures(fixtures_no_seasons)
+    def updateAll(self, no_seasons):
+        """Update all graph files at once.
+
+        Args:
+            no_seasons (int): number of seasons of data to include.
+        """
+        # Fill dataframes
+        # Fixtures for each team
+        self.fixtures = self.getFixtures() 
+        # Standings for the last "n_seasons" seasons
+        self.standings = self.getStandings(no_seasons)
+        # Ratings for each team, based on last "no_seasons" seasons standings table
+        self.team_ratings = self.getTeamRatings(no_seasons, self.standings)
+        
+        # Update graphs
+        self.updateFixtures(no_seasons, self.standings, self.fixtures, self.team_ratings)
+        self.updateHomeAdvantage()
 
 
 if __name__ == "__main__":
     data = Data(2020)
     
-    data.updateFixtures(3)
+    data.updateAll(3)
 
--- gen_data_vis.py
+++ gen_data_vis.py
@@ -2,29 +2,61 @@ import plotly
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from datetime import datetime 
 
 class GenDataVis:
-    def genFixturesGraph(self, team_name, fixtures, team_ratings):
+    def genFixturesGraph(self, team_name, fixtures, team_ratings, home_advantage):
         team_fixtures = fixtures[team_name]
-                
+            
+        now = datetime.now()
+        sizes = [15] * len(fixtures)
         x, y, teams = [], [], []
-        for match in team_fixtures:
+        for i, match in enumerate(team_fixtures):
             x.append(match['Date'])
             rating = team_ratings.loc[team_ratings['Team'] == match['Team']].iloc[0]['Total Rating']
+            # Decrease other team's rating if you're playing at home
+            if match['HomeAway'] == 'Home':
+                rating *= (1 - home_advantage)
             y.append(rating)
-            teams.append(match['Team'])
+            teams.append(match['Team'] + " (" + match['HomeAway'] + ")")
             
+
+            
+            # Increase size of point marker if it's the current upcoming match
+            # now = datetime(2020, 11, 6)
+            # now = datetime(2021, 5, 23)
+            if i == 0:
+                if now < match['Date']:
+                    sizes[i] = 30
+            elif i != len(team_fixtures) and x[-2] < now < match['Date']:
+                sizes[i] = 30
+            
+        y = list(map(lambda x : x*100, y))  # Convert to percentages
         df = pd.DataFrame({'Date': x, 'Ratings': y, 'Teams': teams})
         
         # fig = px.line(df, x="Date", y="Match", color='country')
-        colour_scale = ['#15f00a', '#19c910',  '#18a811', '#137a0d', '#0d5209', '#000000', '#85160f', '#5b1d15', '#ad1a10', '#db1a0d', '#fc1303']
+        colour_scale = ['#01c626', '#08a825',  '#0b7c20', '#0a661b', '#064411', '#000000', '#85160f', '#5b1d15', '#ad1a10', '#db1a0d', '#fc1303']
         fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers', 
-                                        marker=dict(size=15,
+                                        marker=dict(size=sizes,
                                                     color=y,
                                                     colorscale=colour_scale),
                                         line=dict(color='#737373'),
                                         text=teams,
-                                        hoverinfo=('y+text')))
+                                        hovertemplate="<b>%{text}</b> <br>%{x|%d %B, %Y}<br>Rating: %{y:.2f}%<extra></extra>",
+                                        hoverinfo=('x+y+text'),
+                                        ))
+
+        fig.add_shape(go.layout.Shape(type="line",
+                                      yref="paper",
+                                      xref="x",
+                                      x0=now,
+                                      y0=0.04,
+                                      x1=now,
+                                      y1=1.01,
+                                      #line=dict(color="RoyalBlue", width=3),),
+                                      line=dict(color="black", 
+                                                width=1,
+                                                dash="dot")))
         
         # fig = px.scatter(df, x='Date', y='Ratings', labels={'x':'Date', 'y':'Team Rating'},
         #                   color=y, color_continuous_scale=colour_scale, 
@@ -44,11 +76,33 @@ class GenDataVis:
         
         
         # fig.update_layout(annotations=annotations)
-        fig.update_layout({
-        'plot_bgcolor': 'white',
-        'paper_bgcolor': 'white',
-        })
-        fig.update_yaxes(showgrid=True, gridwidth=0.5, gridcolor='gray')
+        # fig.update_layout({
+        #     'plot_bgcolor': '#fafafa',
+        #     'paper_bgcolor': '#fafafa',
+        # })
+        fig.update_layout(
+            yaxis=dict(
+                title_text="Team Rating %",
+                ticktext=[str(i) + "%" for i in range(0, 101, 10)],
+                tickvals=[i for i in range(0, 101, 10)],
+                gridcolor='gray',
+                showline=False,
+                # color="black"
+                zeroline=False,
+            ),
+            xaxis=dict(
+                title_text="Date",
+                linecolor="black",
+                showgrid=False,
+                showline=False,
+            ),
+            plot_bgcolor='#fafafa',
+            paper_bgcolor='#fafafa',
+        )
+        
+        # fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='gray')
+        # fig.update_xaxes(showgrid=False)
+
         # fig.show()
         # Convert team name into suitable use for filename
         file_team_name = '_'.join(team_name.lower().split()[:-1])
