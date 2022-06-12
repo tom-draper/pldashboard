b8e8f7691a8da0b1595e8db95c31183df636090a
Tom Draper <tomjdraper1@gmail.com>
2022-06-12 16:08:50 +0100

    frontend routing

---
+++ frontend/pldashboard2/src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  // import Header from '$lib/header/Header.svelte';
+  import '../app.css';
+</script>
+
+<!-- <Header /> -->
+
+<main>
+  <slot />
+</main>
+
+<!-- <footer>
+  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer> -->
+
+<style>
+
+</style>
