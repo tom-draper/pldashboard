8eb8879f6d561e8b77dcf70d25e1fc3868656196
Tom Draper <tomjdraper1@gmail.com>
2022-08-02 17:31:32 +0100

    Refactor

--- frontend/src/routes/Team.svelte
+++ frontend/src/routes/Team.svelte
@@ -13,7 +13,7 @@
   import CleanSheets from "../components/goals_scored_and_conceded/CleanSheets.svelte";
   import GoalFrequencies from "../components/goals_per_game/GoalsPerGame.svelte";
   import Spider from "../components/Spider.svelte";
-import NavBar from "../components/NavBar.svelte";
+  import NavBar from "../components/NavBar.svelte";
 
   function toTitleCase(str) {
     return str
@@ -30,8 +30,12 @@ import NavBar from "../components/NavBar.svelte";
     if (Object.keys(data.form[data.currentSeason][fullTeamName]).length == 0) {
       return null; // Season has not started yet
     }
-    return Object.keys(data.form[data.currentSeason][fullTeamName]).reduce((a, b) =>
-      data.form[data.currentSeason][fullTeamName][a] > data.form[data.currentSeason][fullTeamName][b] ? a : b
+    return Object.keys(data.form[data.currentSeason][fullTeamName]).reduce(
+      (a, b) =>
+        data.form[data.currentSeason][fullTeamName][a] >
+        data.form[data.currentSeason][fullTeamName][b]
+          ? a
+          : b
     );
   }
 
@@ -71,144 +75,144 @@ import NavBar from "../components/NavBar.svelte";
 <Router>
   <div id="team">
     <div id="nav-bar">
-      <NavBar thisTeam={team}/>
+      <NavBar thisTeam={team} />
     </div>
-  <div class="dashboard">
-    <div class="header" style="background-color: var(--{team});">
-      <a href="/{team}">
-        <div
-          class="main-link title no-decoration"
-          style="color: var(--{team + '-secondary'});"
-        >
-        {fullTeamName}
+    <div class="dashboard">
+      <div class="header" style="background-color: var(--{team});">
+        <a href="/{team}">
+          <div
+            class="main-link title no-decoration"
+            style="color: var(--{team + '-secondary'});"
+          >
+            {fullTeamName}
+          </div>
+        </a>
+      </div>
+
+      {#if data != undefined}
+        <div class="page-content">
+          <div class="row multi-element-row">
+            {#if showBadge}
+              <div
+                class="row-left position-and-badge"
+                style="background-image: url('{data.logoURLs[fullTeamName]}')"
+              >
+                <div class="position">
+                  {data.standings[fullTeamName][data.currentSeason].position}
+                </div>
+              </div>
+            {:else}
+              <div class="row-left position-no-badge">
+                <div class="circles-background-container">
+                  <svg class="circles-background">
+                    <circle
+                      cx="300"
+                      cy="150"
+                      r="100"
+                      stroke-width="0"
+                      fill="var(--{team}-secondary)"
+                    />
+                    <circle
+                      cx="170"
+                      cy="170"
+                      r="140"
+                      stroke-width="0"
+                      fill="var(--{team})"
+                    />
+                    <circle
+                      cx="300"
+                      cy="320"
+                      r="170"
+                      stroke-width="0"
+                      fill="var(--{team})"
+                    />
+                  </svg>
+                </div>
+                <div class="position-central">
+                  {data.standings[fullTeamName][data.currentSeason].position}
+                </div>
+              </div>
+            {/if}
+            <div class="row-right fixtures-graph row-graph">
+              <h1 class="lowered">Fixtures</h1>
+              <div class="graph mini-graph">
+                <Fixtures {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <div class="row multi-element-row">
+            <div class="row-left form-details">
+              <CurrentForm {data} {currentMatchday} {fullTeamName} />
+              <TableSnippet {data} {team} {fullTeamName} />
+            </div>
+            <div class="row-right">
+              <NextGame {data} {currentMatchday} {fullTeamName} {showBadge} />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="form-graph row-graph">
+              <h1 class="lowered">Form Over Time</h1>
+              <div class="graph full-row-graph">
+                <FormOverTime {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="position-over-time-graph row-graph">
+              <h1 class="lowered">Position Over Time</h1>
+              <div class="graph full-row-graph">
+                <PositionOverTime {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <div class="row no-bottom-margin">
+            <div class="goals-scored-vs-conceded-graph row-graph">
+              <h1 class="lowered">Goals Scored and Conceded</h1>
+              <div class="graph full-row-graph">
+                <GoalsScoredAndConceded {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="row-graph">
+              <div class="clean-sheets graph full-row-graph">
+                <CleanSheets {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <div class="season-stats-row">
+            <SeasonStats {data} {fullTeamName} />
+          </div>
+
+          <div class="row">
+            <div class="goals-freq-row row-graph">
+              <h1>Goals Per Game</h1>
+              <GoalFrequencies {data} {fullTeamName} />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="spider-chart-row row-graph">
+              <div class="spider-chart-container">
+                <Spider {data} {fullTeamName} />
+              </div>
+            </div>
+          </div>
+
+          <TeamsFooter lastUpdated={data.lastUpdated} />
         </div>
-      </a>
+      {:else}
+        <div class="loading-spinner-container">
+          <div class="loading-spinner" />
+        </div>
+      {/if}
     </div>
-  
-    {#if data != undefined}
-      <div class="page-content">
-        <div class="row multi-element-row">
-          {#if showBadge}
-            <div
-              class="row-left position-and-badge"
-              style="background-image: url('{data.logoURLs[fullTeamName]}')"
-            >
-              <div class="position">
-                {data.standings[fullTeamName][data.currentSeason].position}
-              </div>
-            </div>
-          {:else}
-            <div class="row-left position-no-badge">
-              <div class="circles-background-container">
-                <svg class="circles-background">
-                  <circle
-                    cx="300"
-                    cy="150"
-                    r="100"
-                    stroke-width="0"
-                    fill="var(--{team}-secondary)"
-                  />
-                  <circle
-                    cx="170"
-                    cy="170"
-                    r="140"
-                    stroke-width="0"
-                    fill="var(--{team})"
-                  />
-                  <circle
-                    cx="300"
-                    cy="320"
-                    r="170"
-                    stroke-width="0"
-                    fill="var(--{team})"
-                  />
-                </svg>
-              </div>
-              <div class="position-central">
-                {data.standings[fullTeamName][data.currentSeason].position}
-              </div>
-            </div>
-          {/if}
-          <div class="row-right fixtures-graph row-graph">
-            <h1 class="lowered">Fixtures</h1>
-            <div class="graph mini-graph">
-              <Fixtures {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <div class="row multi-element-row">
-          <div class="row-left form-details">
-            <CurrentForm {data} {currentMatchday} {fullTeamName} />
-            <TableSnippet {data} {team} {fullTeamName} />
-          </div>
-          <div class="row-right">
-            <NextGame {data} {currentMatchday} {fullTeamName} {showBadge}/>
-          </div>
-        </div>
-  
-        <div class="row">
-          <div class="form-graph row-graph">
-            <h1 class="lowered">Form Over Time</h1>
-            <div class="graph full-row-graph">
-              <FormOverTime {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <div class="row">
-          <div class="position-over-time-graph row-graph">
-            <h1 class="lowered">Position Over Time</h1>
-            <div class="graph full-row-graph">
-              <PositionOverTime {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <div class="row no-bottom-margin">
-          <div class="goals-scored-vs-conceded-graph row-graph">
-            <h1 class="lowered">Goals Scored and Conceded</h1>
-            <div class="graph full-row-graph">
-              <GoalsScoredAndConceded {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <div class="row">
-          <div class="row-graph">
-            <div class="clean-sheets graph full-row-graph">
-              <CleanSheets {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <div class="season-stats-row">
-          <SeasonStats {data} {fullTeamName} />
-        </div>
-  
-        <div class="row">
-          <div class="goals-freq-row row-graph">
-            <h1>Goals Per Game</h1>
-            <GoalFrequencies {data} {fullTeamName} />
-          </div>
-        </div>
-  
-        <div class="row">
-          <div class="spider-chart-row row-graph">
-            <div class="spider-chart-container">
-              <Spider {data} {fullTeamName} />
-            </div>
-          </div>
-        </div>
-  
-        <TeamsFooter lastUpdated={data.lastUpdated} />
-      </div>
-    {:else}
-      <div class="loading-spinner-container">
-        <div class="loading-spinner" />
-      </div>
-    {/if}
-  </div>
   </div>
 </Router>
 
@@ -216,7 +220,54 @@ import NavBar from "../components/NavBar.svelte";
   #team {
     display: flex;
   }
+  .position-and-badge {
+    height: 500px;
+    background-repeat: no-repeat;
+    background-size: auto 450px;
+    background-position: right center;
+  }
 
+  .position-no-badge {
+    padding-left: 0;
+    margin: 0;
+    height: 500px;
+    /* width: 860px; */
+  }
+
+  .position-central,
+  .position {
+    text-shadow: 9px 9px #000;
+    font-weight: 800;
+    font-size: 430px;
+    user-select: none;
+    max-width: 500px;
+  }
+
+  .position {
+    text-align: left;
+    margin-top: 0.02em;
+    margin-left: 30px;
+  }
+
+  .position-central {
+    text-align: center;
+    margin-top: 0.04em;
+    max-height: 500px;
+    margin-left: 0.05em;
+  }
+
+  .circles-background-container {
+    position: absolute;
+    align-self: center;
+    width: 500px;
+    z-index: -10;
+  }
+
+  .circles-background {
+    height: 500px;
+    width: 500px;
+    transform: scale(0.95);
+  }
   .position-central {
     font-size: 23vw;
     margin: auto;
@@ -255,107 +306,111 @@ import NavBar from "../components/NavBar.svelte";
     margin: 1em auto auto;
     display: flex;
   }
-  
-@media only screen and (max-width: 1800px) { 
-  .circles-background {
-    transform: scale(0.9);
-  }
-  .position-central {
-    font-size: 25vw;
-  }
-}
-@media only screen and (max-width: 1600px) { 
-  .row-left {
-    flex: 5;
-  }
-  .circles-background {
-    transform: scale(0.85);
-  }
-}
-@media only screen and (max-width: 1500px) { 
-  .circles-background {
-    transform: scale(0.8);
-  }
-}
-@media only screen and (max-width: 1400px) { 
-  .circles-background {
-    transform: scale(0.75);
-  }
-  #nav-bar {
-    display: none;
-  }
-  .dashboard {
-    margin-left: 0;
-  }
-}
-@media only screen and (max-width: 1300px) { 
-  .circles-background {
-    transform: scale(0.7);
-  }
-}
 
-@media only screen and (max-width: 1200px) { 
-  .circles-background {
-    transform: scale(0.5);
-    margin-top: -80px;
+  @media only screen and (max-width: 1800px) {
+    .circles-background {
+      transform: scale(0.9);
+    }
+    .position-central {
+      font-size: 25vw;
+    }
   }
-}
-@media only screen and (max-width: 1000px) { 
-  .circles-background {
-    transform: scale(0.5);
-    margin-top: -110px;
+  @media only screen and (max-width: 1600px) {
+    .row-left {
+      flex: 5;
+    }
+    .circles-background {
+      transform: scale(0.85);
+    }
   }
-}
+  @media only screen and (max-width: 1500px) {
+    .circles-background {
+      transform: scale(0.8);
+    }
+  }
+  @media only screen and (max-width: 1400px) {
+    .circles-background {
+      transform: scale(0.75);
+    }
+    #nav-bar {
+      display: none;
+    }
+    .dashboard {
+      margin-left: 0;
+    }
+  }
+  @media only screen and (max-width: 1300px) {
+    .circles-background {
+      transform: scale(0.7);
+    }
+  }
 
-@media only screen and (max-width: 800px) {
-  .circles-background {
-    transform: scale(0.45);
-    margin-top: -120px;
+  @media only screen and (max-width: 1200px) {
+    .circles-background {
+      transform: scale(0.5);
+      margin-top: -90px;
+    }
   }
-  .position-central {
-    font-size: 13em;
+  @media only screen and (max-width: 1200px) {
+    .circles-background {
+      margin-top: -100px;
+    }
   }
-  /* .position-no-badge {
+  @media only screen and (max-width: 1000px) {
+    .circles-background {
+      transform: scale(0.5);
+      margin-top: -110px;
+    }
+  }
+
+  @media only screen and (max-width: 800px) {
+    .circles-background {
+      transform: scale(0.45);
+      margin-top: -120px;
+    }
+    .position-central {
+      font-size: 13em;
+    }
+    /* .position-no-badge {
     margin-bottom: -20px;
   } */
-  .season-stats-row {
-    margin: 10px;
-  }
-}
-
-@media only screen and (max-width: 550px) {
-  .position,
-  .position-central {
-    font-size: 10em;
-    text-align: center;
-    line-height: 1.6;
-    padding-right: 20px;
-    margin: 0;
-    text-shadow: 7px 7px #000;
+    .season-stats-row {
+      margin: 10px;
+    }
   }
 
-  .position-and-badge {
-    background-size: auto 210px !important;
-    background-position: center !important;
-  }
+  @media only screen and (max-width: 550px) {
+    .position,
+    .position-central {
+      font-size: 10em;
+      text-align: center;
+      line-height: 1.6;
+      padding-right: 20px;
+      margin: 0;
+      text-shadow: 7px 7px #000;
+    }
 
-  .position-no-badge,
-  .position-and-badge {
-    padding: 0 !important;
-    margin: 0 !important;
-    width: 100%;
-  }
+    .position-and-badge {
+      background-size: auto 210px !important;
+      background-position: center !important;
+    }
 
-  /* .position-and-badge,
+    .position-no-badge,
+    .position-and-badge {
+      padding: 0 !important;
+      margin: 0 !important;
+      width: 100%;
+    }
+
+    /* .position-and-badge,
   .position-no-badge,
   .circles-background-container {
     height: 200px;
   } */
 
-  .circles-background {
-    transform: scale(0.35);
-    margin-top: -125px;
+    .circles-background {
+      transform: scale(0.35);
+      margin-top: -125px;
+    }
   }
-}
-
 </style>
