2519a1331af13b7a55c97f95d6b45e2affd9f949
Tom Draper <tomjdraper1@gmail.com>
2022-10-07 15:30:11 +0100

    Change plotly version

--- frontend/server.js
+++ frontend/server.js
@@ -15,7 +15,7 @@ server.get("*", function(req, res) {
     <link rel='stylesheet' href='/bundle.css'>
     <link rel='icon' href='img/favicon.ico'> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.plot.ly/plotly-2.14.0.min.js" type="text/javascript"></script>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
     <div id="app">${html}</div>
     <script src="/bundle.js"></script>
   `);
