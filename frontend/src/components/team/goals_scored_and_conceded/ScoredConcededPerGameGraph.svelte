d92fbf5a6423df18e91c7a5dd294138e3937c478
Tom Draper <tomjdraper1@gmail.com>
2022-12-13 16:13:20 +0000

    Scored and conceded over time x-axis reformat

--- frontend/src/components/team/goals_scored_and_conceded/ScoredConcededPerGameGraph.svelte
+++ frontend/src/components/team/goals_scored_and_conceded/ScoredConcededPerGameGraph.svelte
@@ -17,7 +17,7 @@
       }
     }
 
-    // Divide by number of teams to get avg goals per gameweek
+    // Divide by number of teams to get avg goals per matchday
     for (let matchday of Object.keys(avgGoals)) {
       avgGoals[matchday] /= 20;
     }
