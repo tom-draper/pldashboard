584f22df8cab182f5633e1c9be1199c59416133c
Tom Draper <tomjdraper1@gmail.com>
2023-08-21 19:35:22 +0100

    fix: html head and body tags

--- frontend/server.js
+++ frontend/server.js
@@ -11,18 +11,22 @@ server.get("*", function(req, res) {
 
   res.write(`
     <!DOCTYPE html>
-    <link rel='stylesheet' href='/global.css'>
-    <link rel='stylesheet' href='/bundle.css'>
-    <link rel='icon' href='img/favicon.ico'> 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="qYIoNrgrvCixUb5A9kCDDt8jvrz7N0x64vPaEgw-Txs" />
-    <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+    <head>
+      <link rel='stylesheet' href='/global.css'>
+      <link rel='stylesheet' href='/bundle.css'>
+      <link rel='icon' href='img/favicon.ico'> 
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="google-site-verification" content="qYIoNrgrvCixUb5A9kCDDt8jvrz7N0x64vPaEgw-Txs" />
+      <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+      <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+    </head>
 
-    <div id="app">${html}</div>
-    <script src="/bundle.js"></script>
+    <body>
+      <div id="app">${html}</div>
+      <script src="/bundle.js"></script>
+    </body>
   `);
 
   res.end();
