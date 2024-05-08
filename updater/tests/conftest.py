a445d574f854e2566d7460962161b90f3c24c1be
Tom Draper <41476809+tom-draper@users.noreply.github.com>
2024-05-08 12:07:33 +0100

    Update conftest.py

--- updater/tests/conftest.py
+++ updater/tests/conftest.py
@@ -1,12 +1,16 @@
 import pytest
 from src.data import Data
 from src.updater import Updater
+from typing import Sequence, Protocol, TypeVar
+
 
 current_season = 2023
 
+# DataFrames built from data backups
 updater_loaded = Updater(current_season)
 updater_loaded.build_all(request_new=False, display_tables=False, update_db=False)
 
+# DataFrames built from live data
 updater_fetched = Updater(current_season)
 updater_fetched.build_all(request_new=True, display_tables=False, update_db=False)
 
@@ -14,7 +18,18 @@ data_objects: list[Data] = [updater_loaded.data, updater_fetched.data]
 data_ids = ["loaded", "fetched"]
 
 
-def is_sorted(my_list):
+class Comparable(Protocol):
+    """Protocol for annotating comparable types."""
+
+    @abstractmethod
+    def __ge__(self: CT, other: CT) -> bool:
+        pass
+
+
+CT = TypeVar("CT", bound=Comparable)
+
+
+def is_sorted(my_list: Sequence[CT]):
     return all(b >= a for a, b in zip(my_list, my_list[1:]))
 
 
